682f5bcac9a08eff2996d940adb017a90796efbe
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-09-04 17:55:41 +0700

    Update setup-proxymanager.sh

--- setup-proxymanager.sh
+++ setup-proxymanager.sh
@@ -3,7 +3,7 @@
 #Install Nginx Proxy Manager on Container
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 sudo apt-get install docker-ce docker-ce-cli containerd.io -y
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
