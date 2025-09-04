ef26c073a93c2a993a5dc43c4b0032138d6199a7
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-09-04 16:46:44 +0700

    Create setup-proxymanager.sh

---
+++ setup-proxymanager.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+#By Anggarda Saputra Wijaya
+#Install Nginx Proxy Manager on Container
+apt update -y
+apt install software-properties-common -y
+apt install ca-certificates curl gnupg -y
+apt update && apt upgrade -y
+apt install docker.io -y
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+mkdir /home/nginx-proxy
+mkdir /home/nginx-proxy/data
+mkdir /home/nginx-proxy/letsencrypt
+cp support/docker-compose.yml /home/nginx-proxy/
+docker network create npm-nw
+docker network create npm-internal
+cd /home/nginx-proxy | docker-compose up -d
+docker ps -a
+ufw allow 80
+ufw allow 81
+ufw allow 443
+ufw enable
+systemctl status ufw
+echo "   ==============================================   ";
+echo "      Dashboard NGINX NPM http://ip-address:81    "
+echo "      Email Login : admin@example.com	 	    "
+echo "      Password    : changeme			    "
+echo "   ==============================================   ";
