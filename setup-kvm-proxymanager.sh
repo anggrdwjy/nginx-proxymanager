03226e9873a688a2e79345a9db91d337970e81ca
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-09-18 00:02:49 +0700

    Update setup-kvm-proxymanager.sh

--- setup-kvm-proxymanager.sh
+++ setup-kvm-proxymanager.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #By Anggarda Saputra Wijaya
 #Install Nginx Proxy Manager on Container
-sudo apt-get update && sudo apt-get upgrade -y
+sudo apt-get update -y
 sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
