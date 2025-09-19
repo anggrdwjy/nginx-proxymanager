de52b722b492810fe00e138fdded5be097f64d74
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-09-19 09:58:16 +0700

    Update setup-kvm-proxymanager.sh

--- setup-kvm-proxymanager.sh
+++ setup-kvm-proxymanager.sh
@@ -2,10 +2,6 @@
 #By Anggarda Saputra Wijaya
 #Install Nginx Proxy Manager on Container
 sudo apt-get update -y
-#sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-#curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg -y
-#echo "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-#sudo apt-get install docker.io docker-ce docker-ce-cli containerd.io -y
 sudo apt-get install curl software-properties-common docker.io -y
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
@@ -20,7 +16,6 @@ cd /home/nginx-proxy
 docker-compose up -d
 docker ps -a
 ufw allow 22
-ufw allow 80
 ufw allow 81
 ufw allow 443
 ufw enable
@@ -29,4 +24,5 @@ echo "   ==============================================   ";
 echo "      Dashboard NGINX NPM http://ip-address:81    "
 echo "      Email Login : admin@example.com	 	    "
 echo "      Password    : changeme			    "
+echo "      !!! PLEASE CHANGE YOUR PASSWORD !!!			    "
 echo "   ==============================================   ";
