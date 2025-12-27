79340668b185b6ee886782dfa7ea2b18736deebd
Anggrdwjy <anggarda.wijaya410@gmail.com>
2025-12-28 00:45:49 +0700

    Update setup-kvm-proxymanager.sh

--- setup-kvm-proxymanager.sh
+++ setup-kvm-proxymanager.sh
@@ -6,17 +6,19 @@ sudo apt-get install curl software-properties-common docker.io -y
 curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 sudo apt-get update -y
+#Portainer
+docker network create portainer_network
 mkdir /home/portainer
+cp support/docker-portainer.yml /home/portainer/docker-compose.yml
+cd /home/portainer
+docker-compose up -d
+#NginxProxyManager
+docker network create npm_network
+docker network create npm_internal
 mkdir /home/nginx-proxy
 mkdir /home/nginx-proxy/data
 mkdir /home/nginx-proxy/letsencrypt
-cp support/docker-portainer.yml /home/portainer/docker-compose.yml
-cp support/docker-nginxproxymanager.yml /home/nginx-proxy/docker-compose.yml
-docker network create npm-nw
-docker network create npm-internal
-docker network create portainer-network
-cd /home/portainer
-docker-compose up -d
+cp support/docker-nginxproxy.yml /home/nginx-proxy/docker-compose.yml
 cd /home/nginx-proxy
 docker-compose up -d
 docker ps -a
@@ -26,9 +28,3 @@ ufw allow 443
 ufw allow 9443
 ufw enable
 systemctl status ufw
-echo "   ==============================================   ";
-echo "      Dashboard NGINX NPM http://ip-address:81    "
-echo "      Email Login : admin@example.com	 	    "
-echo "      Password    : changeme			    "
-echo "      !!! PLEASE CHANGE YOUR PASSWORD !!!			    "
-echo "   ==============================================   ";
